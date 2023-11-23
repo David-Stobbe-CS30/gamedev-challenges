0bfe095b471abcde34aadeeead08f1646c2c9c2d
David Stobbe <d.stobbe1@share.epsb.ca>
2023-11-23 13:28:23 -0700

    8

--- challenge6.py
+++ challenge6.py
@@ -3,7 +3,6 @@ import math
 import pygame
 
 screenSize = [1000, 1000]
-boundaries = [1000, 900]
 pygame.init()
 screen = pygame.display.set_mode((screenSize[0], screenSize[1]))
 
@@ -12,8 +11,8 @@ screen = pygame.display.set_mode((screenSize[0], screenSize[1]))
 
 class Circle:
     def __init__(self):
-        self.x = random.randint(0, boundaries[0])
-        self.y = random.randint(0, boundaries[1])
+        self.x = random.randint(0, screenSize[0])
+        self.y = random.randint(0, screenSize[1])
         self.r = random.randint(5, 25)
         self.xv = random.uniform(0.01, 0.1) * (random.randint(0, 1)*2 - 1)
         self.yv = random.uniform(0.01, 0.1) * (random.randint(0, 1)*2 - 1)
@@ -22,45 +21,59 @@ class Circle:
         pygame.draw.circle(screen, "red", (self.x, self.y), self.r)
 
     def move(self):
-        if (self.x + self.r >= boundaries[0] or self.x - self.r <= 0):
+        if (self.x + self.r >= screenSize[0] or self.x - self.r <= 0):
             self.xv *= -1
 
-        if (self.y + self.r >= boundaries[1] or self.y - self.r <= 0):
+        if (self.y + self.r >= screenSize[1] or self.y - self.r <= 0):
             self.yv *= -1
 
         self.x += self.xv
         self.y += self.yv
 
-
 class Player:
     def __init__(self):
-        self.x = boundaries[0] / 2
-        self.y = boundaries[1] + 50
+        self.x = screenSize[0] / 2
+        self.y = screenSize[1] + 50
         self.r = 20
+        self.orientation = [0, -1]
     def draw(self):
         pygame.draw.circle(screen, "green", (self.x, self.y), self.r)
+        pygame.draw.line(screen, "black", (self.x, self.y), (self.x + (self.orientation[0] * self.r), self.y + (self.orientation[1] * self.r)))
     def shoot(self):
-        bullets.append(Bullet())
+        newBullet = Bullet(self.orientation)
+        bullets.append(newBullet)
     def move(self):
             keys = pygame.key.get_pressed()
             if (keys[pygame.K_a]):
                 self.x -= 1
+                self.orientation = [-1, 0]
             elif (keys[pygame.K_d]):
                 self.x += 1
+                self.orientation = [1, 0]
+            elif (keys[pygame.K_w]):
+                self.y -= 1
+                self.orientation = [0, -1]
+            elif (keys[pygame.K_s]):
+                self.y += 1
+                self.orientation = [0, 1]
 
             if(self.x - self.r < 0):
                 self.x = self.r
-            elif(self.x + self.r > boundaries[0]):
-                self.x = boundaries[0] - self.r
+            elif(self.x + self.r > screenSize[0]):
+                self.x = screenSize[0] - self.r
 
-
+            if(self.y - self.r < 0):
+                self.y = self.r
+            elif(self.y + self.r > screenSize[1]):
+                self.y = screenSize[1] - self.r
 
 class Bullet:
-    def __init__(self):
+    def __init__(self, orientation):
         self.x = player.x
         self.y = player.y
         self.r = 5
         self.hit = False
+        self.orientation = orientation
     def draw(self):
         pygame.draw.circle(screen, "black", (self.x, self.y), self.r)
     def collide(self):
@@ -69,7 +82,8 @@ class Bullet:
                 circles.remove(circle)
                 self.hit = True
     def move(self):
-        self.y -= 1
+        self.y += self.orientation[1]
+        self.x += self.orientation[0]
 
 
 circles = []
@@ -77,6 +91,7 @@ for n in range(10):
     circles.append(Circle())
 player = Player()
 bullets = []
+
 
 def draw():
     for circle in circles:
